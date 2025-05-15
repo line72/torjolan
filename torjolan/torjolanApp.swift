73bd4357c633abd170082f01511c565494d98a5e
Marcus Dillavou <line72@line72.net>
2025-05-15 17:11:29 -0500

    Initial repository

---
+++ torjolan/torjolanApp.swift
@@ -0,0 +1,17 @@
+//
+//  torjolanApp.swift
+//  torjolan
+//
+//  Created by line72 on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct torjolanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ torjolanTests/torjolanTests.swift
@@ -0,0 +1,16 @@
+//
+//  torjolanTests.swift
+//  torjolanTests
+//
+//  Created by line72 on 5/15/25.
+//
+
+import Testing
+
+struct torjolanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
